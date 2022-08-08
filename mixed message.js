529779d6d9e7f172d6d56163e527b5264ccfecd8
Asher G Vongtau <agv239@nyu.edu>
2022-08-08 11:21:34 -0400

    First Commit

---
+++ mixed message.js
@@ -0,0 +1,3 @@
+const messages = ["Robert Lewandowski plays in Spain for FC Barcelona", "Lionel Messi plays in France for Paris St. Germain", "Cristiano Ronaldo plays in England for Manchester United", "Manuel Neuer plays in Germany for FC Bayern Munich", "Gareth Bale plays in The United States of America for LA Galaxy FC"]
+const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+console.log(randomMessage)
